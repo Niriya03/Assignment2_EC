8d911f1893184b7dcd7e2c75c83b015993a0a1ab
Priyanka Chopra <Priyanka.Chopra@edu.sait.ca>
2026-04-02 00:01:07 -0600

    feat: create React app showing Assignment2

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Assignment2 on the page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const nameElement = screen.getByText(/Assignment2/i);
+  expect(nameElement).toBeInTheDocument();
 });
